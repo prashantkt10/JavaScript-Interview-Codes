27fcad395c6a018e08fdec0883a4fe4cf632b4ac
Prashant Kumar Tripathi <prashantkt10@gmail.com>
2019-08-11 15:43:11 +0530

    array-chunking solution-1

--- array-chuncking/index.js
+++ array-chuncking/index.js
@@ -1,6 +1,16 @@
-function chunkarray(array) {
+//Method-1
+function chunkarray(array, size) {
     let tempArray = [];
-    for (let x = 0; x < array[0].length; x++) {
-
+    for (let x = 0; x < array.length;) {
+        let internalArray = [];
+        for (y = x; y < x + size; y++) {
+            if (array[y])
+                internalArray.push(array[y]);
+        }
+        tempArray.push(internalArray);
+        x = x + size;
     }
+    console.log(tempArray);
 }
+
+chunkarray([1, 2, 3, 4, 5], 3);
