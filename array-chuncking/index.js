0102af785e38d4a2f3ee244c6a9e3fed73188f20
Prashant Kumar Tripathi <prashantkt10@gmail.com>
2019-08-11 19:00:33 +0530

    With more chunk solutions

--- array-chuncking/index.js
+++ array-chuncking/index.js
@@ -1,4 +1,4 @@
-//Method-1
+Method - 1
 function chunkArray(array, size) {
     let tempArray = [];
     for (let x = 0; x < array.length;) {
@@ -15,7 +15,9 @@ function chunkArray(array, size) {
 
 chunkArray([1, 2, 3, 4, 5], 3);
 
-//Method-2
+
+
+Method - 2
 function chunkArray(array, size) {
     let chunked = [];
 
@@ -29,6 +31,22 @@ function chunkArray(array, size) {
     }
     console.log(chunked);
     return chunked;
+}
+
+chunkArray([1, 2, 3, 4, 5], 4);
+
+
+
+//Method-3
+function chunkArray(array, size) {
+    const chunked = [];
+    let index = 0;
+
+    while (index < array.length) {
+        chunked.push(array.slice(index, index + size));
+        index += size;
+    }
+    console.log(chunked);
 }
 
 chunkArray([1, 2, 3, 4, 5], 4);
